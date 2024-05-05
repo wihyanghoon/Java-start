eaa265ad3d06089af4473ba481be953389ac14a4
WIE HYANG HOON <gidgns1995@gmail.com>
2024-05-05 23:53:21 +0900

    [add] array 배열선언 방법

--- src/array/Array1Ref2.java
+++ src/array/Array1Ref2.java
@@ -3,15 +3,7 @@ package array;
 public class Array1Ref2 {
     public static void main(String[] args) {
         // 배열 변수선언
-        int[] students;
-        students = new int[5];
-
-        // 변수 값 할당
-        students[0] = 90;
-        students[1] = 80;
-        students[2] = 70;
-        students[3] = 60;
-        students[4] = 50;
+        int[] students = new int[]{90,80,70,60,50};
 
         // 배열 출력
         for (int i = 0; i < students.length; i++){
